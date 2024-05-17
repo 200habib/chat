43bcadebb1e0492426562ce94da9a5737d72863f
el habib <elhabib10benaboud@gmail.com>
2024-05-17 12:51:48 +0200

    fpppppppppp

--- queries SQL/add_query.php
+++ queries SQL/add_query.php
@@ -2,28 +2,28 @@
 require_once '../config/config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-try {
-    $task = cleanInput($_POST['task'] ?? '');
-    if (empty($task)) {
-        echo "The title is required.";
-    }
-    $stmt = $pdo->prepare("INSERT INTO messages (message, create_at) VALUES (?, NOW())");
-
-    if ($stmt) {
-        $stmt->bindParam(1, $task);
-        if ($stmt->execute()) {
-            header("Location: ../public/index.php");
+    try {
+        $task = trim(htmlspecialchars($_POST['task'] ?? ''));
+        if (empty($task)) {
+            echo "Error";
             exit;
-        } else {
-
-            echo "Error.";
         }
-    } else {
-        echo "Error.";
+
+        $stmt = $pdo->prepare("INSERT INTO messages (message, create_at) VALUES (?, NOW())");
+
+        if ($stmt) {
+            $stmt->bindParam(1, $task);
+            if ($stmt->execute()) {
+                header("Location: ../public/index.php");
+                exit;
+            } else {
+                echo "Error";
+            }
+        } else {
+            echo "Error";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "Error" . $e->getMessage();
-}
 }
 ?>
